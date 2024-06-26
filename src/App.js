d1649a62f01b1a0fb6926a3e5e30e18b35c60f7e
thissurya123 <thissurya31@gmail.com>
2024-06-26 22:07:00 +0700

    add Win:feature

--- src/App.js
+++ src/App.js
@@ -11,13 +11,38 @@ function Square({value, onSquareclick}){
 export default function App(){
   const [change, setChange] = useState(true);
   const [square, setSquare] = useState(Array(9).fill(null));
-  
+  function resetBtn(){
+    setSquare(Array(9).fill(null));
+  }
+
   function handleClick(i){
     const nextSquare = square.slice();
+    //cek Pemenang
+    const menang = [
+      [0,1,2],
+      [0,4,8],
+      [0,3,6],
+      [3,4,5],
+      [6,7,8],
+      [6,4,2]
+    ];
+
+    for(let i= 0; i < menang.length; i++){
+      const [a, b, c] = menang[i];
+      if(square[a] && square[a] === square[b] && square[a] === square[c]){
+        alert(square[i] + "you win");
+        return
+      }
+    }
+
+    if(square[i]){
+      return
+    }
+
     if(change){
-      nextSquare[i] = 'X';
+      nextSquare[i] = "O";
     }else{
-      nextSquare[i] = 'O';
+      nextSquare[i] = "X";
     }
     setSquare(nextSquare);
     setChange(!change);
@@ -25,6 +50,7 @@ export default function App(){
 
   return(
     <div className="md:w-5/12 mx-auto text-white font-bold text-5xl mt-24">
+    <button className="bg-blue-500 p-2" onClick={resetBtn}>Reset</button>
     <div className="flex border bg-gray-900">
         <Square value={square[0]} onSquareclick={() => {handleClick(0)}} />
         <Square value={square[1]} onSquareclick={() => {handleClick(1)}} />
